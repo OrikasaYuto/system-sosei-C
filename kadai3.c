7c61d4f7aff45fd17753e1ae1bbd4329c994ba6c
OrikasaYuto <148538259+OrikasaYuto@users.noreply.github.com>
2023-11-10 18:55:40 +0900

    Create kadai3.c

---
+++ kadai3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int max = 10000;
+	for (int i = 1; i <= 10000; i++)
+	{
+		if (i % 13 == 0)
+		{
+			printf("%d\n", i);
+		}
+		else if (i % 17 == 0)
+		{
+			printf("%d\n", i);
+		}
+	}
+	
+	return 0;
+}
